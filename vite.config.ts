251207637c87751eadf2ad68e0f82d6c186ba16c
Hussein <husseinalmuhanna02@gmail.com>
2026-06-30 05:22:55 +0300

    Fix: Ignore rollup warnings and force production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return;
+        warn(warning);
+      }
+    }
   }
-})
+});
