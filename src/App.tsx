095490d671d18491792677b33fc8ca290dae85f1
husseinalmuhanna02-creator <husseinalmuhanna02@gmail.com>
2026-08-03 12:15:15 +0300

    Fix SVG path formatting in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -920,7 +920,7 @@ function VerifiedBadge({ isVerified, size = "sm" }: { isVerified?: boolean; size
   const iconSize = size === "sm" ? "w-4 h-4" : "w-5 h-5";
   return (
     <svg className={`${iconSize} text-blue-500 fill-current inline-block shrink-0`} viewBox="0 0 24 24">
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
     </svg>
   );
 }
