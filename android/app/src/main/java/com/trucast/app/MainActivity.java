b9f59756c6b5d8de8548f3ba26c60df8a5201ba7
husseinalmuhanna02-creator <husseinalmuhanna02@gmail.com>
2026-08-03 13:41:54 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/trucast/app/MainActivity.java
+++ android/app/src/main/java/com/trucast/app/MainActivity.java
@@ -1,13 +1,27 @@
-package com.trucast.app;
+package com.hussein.trucast;
 
 import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // هذا هو السطر السحري الذي سيفعل إضافة مشاركة الشاشة
-        registerPlugin(ScreenSharePlugin.class);
         super.onCreate(savedInstanceState);
+
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+                @Override
+                public void onPermissionRequest(final PermissionRequest request) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            request.grant(request.getResources());
+                        }
+                    });
+                }
+            });
+        }
     }
 }
