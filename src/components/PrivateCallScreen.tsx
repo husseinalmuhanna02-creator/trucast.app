a857384ebb05a7398f1190dbc8595b0f20244071
husseinalmuhanna02-creator <husseinalmuhanna02@gmail.com>
2026-08-15 03:03:34 +0300

    Import getDoc from firebase/firestore

--- src/components/PrivateCallScreen.tsx
+++ src/components/PrivateCallScreen.tsx
@@ -3,7 +3,7 @@ import { useLanguage } from '../localization';
 import { User as FirebaseUser } from 'firebase/auth';
 import { Chat, CallSession } from '../types';
 import { db, auth } from '../firebase';
-import { doc, updateDoc, serverTimestamp, addDoc, collection } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, serverTimestamp, addDoc, collection } from 'firebase/firestore';
 import {
   StreamVideo,
   StreamCall,
